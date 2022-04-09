afb87a19ecbb12f935cc68f10531ba60c3d855d3
SeungYongShim <syshim@me.com>
2022-04-09 14:22:43 +0900

    코드 수정

--- src/webapi/Program.cs
+++ src/webapi/Program.cs
@@ -1,21 +1,11 @@
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.Configure<KestrelServerOptions>(options =>
-{
-   // options.AllowSynchronousIO = true;
-});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -31,8 +21,6 @@ app.Use((context, next)=>
 });
 
 app.MapControllers();
-
-
 
 app.Run();
 
